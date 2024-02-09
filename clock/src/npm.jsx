36e433ffb3d8d588f52bb8470ad86125340473e3
mansiijoshii <mansijoshi2703@gmail.com>
2024-02-09 18:46:32 +0530

    Final changes

--- clock/src/npm.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Clock from 'react-clock';
-
-const Func=()=>{
-    <Clock/>;
-
-};
-export default Func;
--- pokemon/src/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home=()=>{
-   return( <h1>home page</h1>);
-}
-
-export default Home;

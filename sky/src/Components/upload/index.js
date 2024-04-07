1bdfc8e811ac3b710c4f592a4e9ae077d6095f18
Somendra Aggarwal <somendra.nhn@gmail.com>
2024-04-07 11:21:13 +0530

    mask page

--- sky/src/Components/upload/index.js
+++ sky/src/Components/upload/index.js
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import backimg from "../Assets/backimg.png";
 import img from "../Assets/photo.png";
-import { useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Upload = () => {
   const navigate = useNavigate();
-
   const inputRef = useRef(null);
   const [image, setImage] = useState("");
+
   const handleImageClick = () => {
     inputRef.current.click();
   };
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
-    console.log(file);
-    setImage(event.target.files[0]);
+    setImage(URL.createObjectURL(file)); // Save uploaded image URL
   };
 
   return (
@@ -30,7 +28,7 @@ const Upload = () => {
       >
         {image ? (
           <img
-            src={URL.createObjectURL(image)}
+            src={image}
             style={{ height: "60%", width: "50%" }}
             alt="Image"
           />
@@ -39,12 +37,9 @@ const Upload = () => {
         )}
         <input type="file" onChange={handleImageChange} ref={inputRef} />
       </div>
-
       <div className="flex flex-col items-center mt-[50px]">
         <button
-          onClick={() => {
-            navigate("/mask");
-          }}
+          onClick={() => navigate(`/mask?image=${encodeURIComponent(image)}`)}
           className=' items-center button type="button" class="text-white bg-gradient-to-r from-teal-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-full text-sm px-5 py-2.5 items-center me-2 mb-2"'
         >
           Get Mask

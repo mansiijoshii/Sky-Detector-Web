3c109309784c6001210dcc38b621a0f1edc30875
Somendra Aggarwal <somendra.nhn@gmail.com>
2024-04-08 19:27:00 +0530

    bg-change

--- sky/src/Components/header/index.js
+++ sky/src/Components/header/index.js
@@ -1,48 +1,54 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../contexts/authContext'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/authContext";
 //import { doSignOut } from '../../firebase/auth'
-import { signOut, getAuth } from 'firebase/auth'
+import { signOut, getAuth } from "firebase/auth";
 
 const Header = () => {
-    const navigate = useNavigate()
-    const { userLoggedIn } = useAuth()
-    const auth= getAuth();
-    const handlesignout=()=>{
-        signOut(auth)
-        .then(() => { navigate('/login')})
-        .catch((error)=>console.log("error"));
+  const navigate = useNavigate();
+  const { userLoggedIn } = useAuth();
+  const auth = getAuth();
+  const handlesignout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((error) => console.log("error"));
+  };
+  return (
+    <nav className="flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-#2ABFC8 ">
+      {userLoggedIn ? (
+        <>
+          <div>
+            <i class="fa-brands fa-skyatlas"></i>
+          </div>
+          <ul className="flex">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About Us</Link>
+            </li>
+          </ul>
+          <button
+            onClick={handlesignout}
+            className="text-white bg-gradient-to-r from-[#2A4E77] to-[#7EA1CA] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-[#2A4E77] dark:focus:ring-[#2A4E77] shadow-lg shadow-[#7EA1CA] dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link className="text-sm text-blue-600 underline" to={"/login"}>
+            Login
+          </Link>
+          <Link className="text-sm text-blue-600 underline" to={"/register"}>
+            Register New Account
+          </Link>
+        </>
+      )}
+    </nav>
+  );
+};
 
-    };
-    return (
-        <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-#2ABFC8 '>
-       
-            {
-                userLoggedIn
-                    ?
-                    <> 
-                    <div> 
-                     <i class="fa-brands fa-skyatlas"></i>
-                   </div> 
-                    <ul className='flex'>
-                        <li>
-                        <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                        <Link to="/About">About Us</Link>
-                        </li>
-                    </ul>
-                        <button onClick={handlesignout} className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>
-                    </>
-                    :
-                    <>
-                        <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
-                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
-                    </>
-            }
-
-        </nav>
-    )
-}
-
-export default Header
+export default Header;
--- sky/src/Components/upload/index.js
+++ sky/src/Components/upload/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import backimg from "../Assets/backimg.png";
-import img from "../Assets/photo.png";
+import bg from "../Assets/bg.png";
+import img from "../Assets/ShareIcon.png";
 
 const Upload = () => {
   const navigate = useNavigate();
@@ -20,7 +20,7 @@ const Upload = () => {
   return (
     <div
       className="w-full h-screen bg-no-repeat bg-cover"
-      style={{ backgroundImage: `url(${backimg})` }}
+      style={{ backgroundImage: `url(${bg})` }}
     >
       <div
         onClick={handleImageClick}
@@ -33,14 +33,14 @@ const Upload = () => {
             alt="Image"
           />
         ) : (
-          <img src={img} style={{ height: "40%", width: "25%" }} alt="Img" />
+          <img src={img} style={{ height: "30%", width: "20%" }} alt="Img" />
         )}
         <input type="file" onChange={handleImageChange} ref={inputRef} />
       </div>
       <div className="flex flex-col items-center mt-[50px]">
         <button
           onClick={() => navigate(`/mask?image=${encodeURIComponent(image)}`)}
-          className=' items-center button type="button" class="text-white bg-gradient-to-r from-teal-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-full text-sm px-5 py-2.5 items-center me-2 mb-2"'
+          className='button type = "button" class=" text-white bg-gradient-to-r from-[#2A4E77] to-[#6587AE] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-[#2A4E77] dark:focus:ring-[#7EA1CA] shadow-lg shadow-[#6587AE] dark:shadow-lg font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ml-4 pl-2"'
         >
           Get Mask
         </button>
